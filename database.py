96a9f983310ad9e6ea79837e738f5e7a9596a505
co2plant <93631841+co2plant@users.noreply.github.com>
2024-06-09 19:17:58 +0900

    # 데이터베이스 및 수정관련.

--- database.py
+++ database.py
@@ -7,6 +7,66 @@ with open('config.json') as f:
     USER = data['MARIA']['USER']
     PASSWORD = data['MARIA']['PASSWORD']
     DB = data['MARIA']['DB']
+def insert_standings(game_info):
+    # Connect to the database
+    conn = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=DB, charset='utf8')
+    cursor = conn.cursor()
+
+    # Insert into Standings table
+    insert_standings_query = """
+    INSERT INTO Standings (id, team, win, lose, draw, rate, last_10, streak, home, away)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    standings_values = (game_info[0], game_info[1], game_info[2], game_info[3], game_info[4], game_info[5], game_info[6], game_info[7], game_info[8], game_info[9])
+    try:
+        cursor.execute(insert_standings_query, standings_values)
+    except:
+        print(f"Error inserting standings: {standings_values}")
+
+    # Commit and close
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def update_standings(game_info):
+    # Connect to the database
+    conn = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=DB, charset='utf8')
+    cursor = conn.cursor()
+
+    # Insert into Standings table
+    update_standings_query = """
+    UPDATE Standings SET team = %s, win = %s, lose = %s, draw = %s, rate = %s, last_10 = %s, streak = %s, home = %s, away = %s WHERE id = %s
+    """
+    standings_values = (game_info[1], game_info[2], game_info[3], game_info[4], game_info[5], game_info[6], game_info[7], game_info[8], game_info[9]. game_info[0])
+    try:
+        cursor.execute(update_standings_query, standings_values)
+    except:
+        print(f"Error inserting standings: {standings_values}")
+
+    # Commit and close
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def select_standings():
+    conn = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=DB, charset='utf8')
+    cursor = conn.cursor()
+
+    select_query = """
+    SELECT * FROM Standings
+    """
+
+    try:
+        cursor.execute(select_query)
+        result = cursor.fetchall()
+    except:
+        result = None
+
+    cursor.close()
+    conn.close()
+
+    return result
+
 
 def insert_game_and_score(game_info):
     # Connect to the database
--- kbo_crawler.py
+++ kbo_crawler.py
@@ -3,6 +3,54 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
 
+def insert_standings():
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('no-sandbox')
+
+    driver = webdriver.Chrome(options)
+    driver.get('https://www.koreabaseball.com/Record/TeamRank/TeamRankDaily.aspx')
+
+    standingsArea = driver.find_elements(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_udpRecord"]/table/tbody/tr')
+
+    for row in standingsArea:
+        tds = row.find_elements(By.TAG_NAME, 'td')
+        id = tds[0].text
+        team = tds[1].text
+        win = tds[2].text
+        lose = tds[3].text
+        draw = tds[4].text
+        rate = tds[6].text
+        last_10 = tds[8].text
+        streak = tds[9].text
+        home = tds[10].text
+        away = tds[11].text
+        database.insert_standings([id, team, win, lose, draw, rate, last_10, streak, home, away])
+
+def update_standings():
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('no-sandbox')
+
+    driver = webdriver.Chrome(options)
+    driver.get('https://www.koreabaseball.com/Record/TeamRank/TeamRankDaily.aspx')
+
+    standingsArea = driver.find_elements(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_udpRecord"]/table/tbody/tr')
+
+    for row in standingsArea:
+        tds = row.find_elements(By.TAG_NAME, 'td')
+        id = tds[0].text
+        team = tds[1].text
+        win = tds[2].text
+        lose = tds[3].text
+        draw = tds[4].text
+        rate = tds[6].text
+        last_10 = tds[8].text
+        streak = tds[9].text
+        home = tds[10].text
+        away = tds[11].text
+        database.update_standings([team, win, lose, draw, rate, last_10, streak, home, away, id])
+
 def update_schedule_once(selected_date):
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
@@ -43,7 +91,7 @@ def update_schedule_once(selected_date):
                         score[j]+=word
                     else:
                         team[j]+=word
-            database.insert_game_and_score([selected_date, tds[1].text, team[0], score[0], score[1], team[1], tds[7].text, tds[8].text])
+            database.update_game_and_score([selected_date, tds[1].text, team[0], score[0], score[1], team[1], tds[7].text, tds[8].text])
         else:
             temp = tds[1].text.split('vs')
             for j in range(2):
@@ -52,7 +100,50 @@ def update_schedule_once(selected_date):
                         score[j]+=word
                     else:
                         team[j]+=word
-            database.insert_game_and_score([selected_date, tds[0].text, team[0], score[0], score[1], team[1], tds[6].text, tds[7].text])
+            database.update_game_and_score([selected_date, tds[0].text, team[0], score[0], score[1], team[1], tds[6].text, tds[7].text])
+
+def update_score(selected_date):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('no-sandbox')
+
+    driver = webdriver.Chrome(options)
+    driver.get('https://www.koreabaseball.com/Schedule/Schedule.aspx')
+
+    scheduleArea = driver.find_elements(By.XPATH, '//*[@id="tblScheduleList"]/tbody/tr')
+
+    id_format = None
+    incount = 0
+    temp = None
+
+    for row in scheduleArea:
+        i=1
+        try:
+            temp = row.find_element(By.CLASS_NAME, 'day').text
+            temp = re.split(r'[.|(]', temp)
+            incount = 0
+        except:
+            i=0
+        id_format = temp[0]+temp[1]+str(incount).zfill(2) #무의미한 계산을 줄이기 위해 사용
+
+        separated_row = row.text.split(' ')
+
+        st = separated_row[i+1].split('vs')
+        team = ['', '']
+        score = ['', '']
+
+        for j in range(2):
+            for word in list(st[j]):
+                if word.isdigit():
+                    score[j]+=word
+                else:
+                    team[j]+=word
+
+        if score[0] == '':
+            score[0] = '-1'
+        if score[1] == '':
+            score[1] = '-1'
+
 
 def insert_schedule_month():
     options = webdriver.ChromeOptions()
@@ -103,12 +194,4 @@ def insert_schedule_month():
         database.insert_game_and_score([id_format, separated_row[i], team[0], score[0], score[1], team[1], separated_row[-2], separated_row[-1]])
     driver.quit()
 
-while(True):
-    opcode = input()
-    if opcode == '1':
-        insert_schedule_month()
-        break
-    elif opcode == '2':
-        update_schedule_once(input())
-    else:
-        break
+insert_schedule_month()
