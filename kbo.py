0cc07df203faccad6c6ea751e01b2120c8e798c2
co2plant <93631841+co2plant@users.noreply.github.com>
2024-06-09 15:05:28 +0900

    # 이모지 순서 변경

--- kbo.py
+++ kbo.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import discord
 from discord import app_commands
+from discord.ext import tasks
 
 from typing import Literal, Union, NamedTuple
 from enum import Enum
@@ -17,6 +18,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 import database
+import kbo_crawler
 
 with open('config.json') as f:
     data = json.load(f)
@@ -81,6 +83,41 @@ async def parade_rest(interaction: discord.Interaction):
 async def as_you_were(interaction: discord.Interaction):
     await interaction.response.send_message(f'쉬어!')
 
+@tasks.loop(hour=6)
+async def update_tables():
+    kbo_crawler.update_standings()
+    kbo_crawler.update_schedule_once(datetime.today().strftime('%m%d'))
+
+@client.tree.command(name='순위', description='돌승엽이 KBO 순위를 당신에게 보여줍니다.')
+async def standings(interaction : discord.Interaction):
+    embed = discord.Embed(title='KBO 순위', url='https://sports.news.naver.com/kbaseball/record/index?category=kbo', color=0x00AEEF)
+
+    from_db_result = database.select_standings()
+    if from_db_result is None:
+        await interaction.response.send_message('일정을 찾을 수 없습니다.')
+        return
+
+    str = ''
+    for i in range(len(from_db_result)):
+        str = str + f'{emoji[i+1]} {from_db_result[i][2]}{logo_emoji[from_db_result[i][2]]} {from_db_result[i][7]}승 {from_db_result[i][8]}패 {from_db_result[i][9]}무\n'
+
+    embed_title= [ "순위   팀", "승  패  무  승률", "최근 10경기   연속   홈   방문"]
+    str = ['', '', '']
+    for i in range(len(from_db_result)):
+        str[0] = str[0] + f'{emoji[i+1]:<5} {logo_emoji[from_db_result[i][1]]} {from_db_result[i][1]}\n'
+        str[1] = str[1] + f'{from_db_result[i][2]}' + '  ' + f'{from_db_result[i][3]}' + '  ' + f'{from_db_result[i][4]}' + '  ' + f'{from_db_result[i][5]}' + '\n'
+        str[2] = str[2] + f'{from_db_result[i][6]}'  + '  ' + f'{from_db_result[i][7]:}' + '  ' + f'{from_db_result[i][8]}' + '  ' + f'{from_db_result[i][9]}' +'\n'
+
+    embed.set_footer(text='Created').timestamp = datetime.now()
+    embed.add_field(name=embed_title[0], value=str[0], inline=True)
+    embed.add_field(name=embed_title[1], value=str[1], inline=True)
+    embed.add_field(name=embed_title[2], value=str[2], inline=True)
+
+    embed.set_footer(text='Created').timestamp = datetime.now()
+    embed.add_field(name='팀 순위', value=str, inline=True)
+
+    await interaction.response.send_message(embed=embed)
+
 @client.tree.command(name='일정', description='돌승엽이 KBO 경기 일정을 당신에게 보여줍니다.')
 @app_commands.describe(args_date='[오늘|내일|모레]를 선택해 언제 일정을 확인할지 선택하세요.')
 async def schedule(interaction: discord.Interaction, args_date: Literal['오늘', '내일', '모레']):
@@ -102,7 +139,7 @@ async def schedule(interaction: discord.Interaction, args_date: Literal['오늘'
     for i in range(len(from_db_result)):
         str[0] = str[0] + f'{emoji[i+1]:<5} {from_db_result[i][1]}\n'
         score = [from_db_result[i][7] if from_db_result[i][7] != -1 else '', from_db_result[i][8] if from_db_result[i][8] != -1 else '']
-        str[1] = str[1] + f'{from_db_result[i][2]:<10}{logo_emoji[from_db_result[i][2]]}{score[0]} vs {score[1]}{from_db_result[i][3]:<10}{logo_emoji[from_db_result[i][3]]}\n'
+        str[1] = str[1] + f'{from_db_result[i][2]:<10}{logo_emoji[from_db_result[i][2]]}{score[0]} vs {score[1]}{logo_emoji[from_db_result[i][3]]}{from_db_result[i][3]:<10}\n'
         str[2] = str[2] + f'{from_db_result[i][4]:<5}{from_db_result[i][5]:<5}\n'
 
     embed.set_footer(text='Created').timestamp = datetime.now()
@@ -111,8 +148,5 @@ async def schedule(interaction: discord.Interaction, args_date: Literal['오늘'
     embed.add_field(name=embed_title[2], value=str[2], inline=True)
 
     await interaction.response.send_message(embed=embed)
-
-
-
 
 client.run(TOKEN)
